3d5eca27118920854ed754c2273e9bd02d560a66
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-16 09:25:30 +0100

    *

--- src/learning/java/animals/Animal.java
+++ src/learning/java/animals/Animal.java
@@ -2,7 +2,11 @@ package learning.java.animals;
 
 public abstract class Animal {
     public abstract void eat();
-    public final String eatString = "sto mangiando ";
+    public final String eatString;
+
+    public Animal(){
+        this.eatString = "sto mangiando ";
+    }
 
     public void sleep(){
         System.out.println("ZzzZZZzzzzZ");
